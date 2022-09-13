1ea0ed48b46c5e284e011720788d04406dfefc7e
Adam26114 <86336270+Adam26114@users.noreply.github.com>
2022-09-13 08:01:10 +0630

    14 Sep 2022

---
+++ l14clickposition/app.js
@@ -0,0 +1,10 @@
+const getmap = document.querySelector('.map-container');
+
+var getsmallcolor = document.getElementById('smallcolor'),
+    getmediumcolor = document.getElementById('mediumcolor'),
+    getlargcolor = document.getElementById('largecolor');
+
+getmap.addEventListener('click',function(){
+    console.log('i am working');
+});
+
