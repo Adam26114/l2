58a3f67f081ab1ef674fe3662b9ffd8c1a6bc185
Adam26114 <86336270+Adam26114@users.noreply.github.com>
2022-09-13 08:30:21 +0630

    14 Sep 2022

--- l14clickposition/app.js
+++ l14clickposition/app.js
@@ -4,7 +4,35 @@ var getsmallcolor = document.getElementById('smallcolor'),
     getmediumcolor = document.getElementById('mediumcolor'),
     getlargcolor = document.getElementById('largecolor');
 
-getmap.addEventListener('click',function(){
-    console.log('i am working');
+var circleidx = 0;
+
+getmap.addEventListener('click',function(e){
+    circleidx++;
+
+    // console.log('i am working');
+
+    const cx = e.clientX;
+    const cy = e.clientY;
+    // console.log(cx,cy);
+
+
+    const mapleft = e.target.offsetLeft;
+    const maptop = e.target.offsetTop;
+    // console.log(mapleft,maptop);
+
+    const curx = cx - mapleft;
+    const cury = cy -maptop;
+    // console.log(curx,cury);
+
+    const circle = document.createElement('span');
+    circle.id = circleidx;
+    circle.classList.add('circle');
+    // console.log(circle);
+
+    circle.style.left = `${curx}px`;
+    circle.style.top = `${cury}px`;
+
+    e.target.appenChild(circle);
+
 });
 
