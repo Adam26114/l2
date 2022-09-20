3c7e57b34f9c91ccc7f25d6e00165c8a240ff334
Adam26114 <zakato26114@gmail.com>
2022-09-20 08:19:10 +0630

    20 Sep 2022

--- l15verifyimagebox/app.js
+++ l15verifyimagebox/app.js
@@ -1,63 +1,136 @@
-const database = [
+let database = [
     {
-        question:"Choose Traffic Light ?",
-        a:"./img/traffic.jpg",
-        b:"./img/mountain.jpg",
-        c:"./img/ambulance.jpg",
-        d:"./img/airport.jpg",
-        correctanswer:"a"
+        question : "Choose Traffic Light?",
+        a : "./img/traffic.jpg",
+        b : "./img/mountain.jpg",
+        c : "./img/ambulance.jpg",
+        d : "./img/airport.jpg",
+        correctanswer : "a"
     },
-    
     {
-        question:"Choose Mountain Light ?",
-        a:"./img/ambulance.jpg",
-        b:"./img/mountain.jpg",
-        c:"./img/traffic.jpg",
-        d:"./img/airport.jpg",
-        correctanswer:"b"
+        question : "Choose Mountain?",
+        a : "./img/ambulance.jpg",
+        b : "./img/mountain.jpg",
+        c : "./img/traffic.jpg",
+        d : "./img/airport.jpg",
+        correctanswer : "b"
     },
-    
     {
-        question:"Choose Ambulance Light ?",
-        a:"./img/traffic.jpg",
-        b:"./img/mountain.jpg",
-        c:"./img/ambulance.jpg",
-        d:"./img/airport.jpg",
-        correctanswer:"a"
+        question : "Choose Ambulance Truck",
+        a : "./img/ambulance.jpg",
+        b : "./img/airport.jpg",
+        c : "./img/traffic.jpg",
+        d : "./img/mountain.jpg",
+        correctanswer : "a"
     },
-    
     {
-        question:"Choose Airport Light ?",
-        a:"./img/traffic.jpg",
-        b:"./img/mountain.jpg",
-        c:"./img/ambulance.jpg",
-        d:"./img/airport.jpg",
-        correctanswer:"d"
-    },
-    
-]
+        question : "Choose Airport?",
+        a : "./img/traffic.jpg",
+        b : "./img/mountain.jpg",
+        c : "./img/ambulance.jpg",
+        d : "./img/airport.jpg",
+        correctanswer : "d"
+    }
+];
 
-// console.log(database[0].b);
+
+// console.log(database);
+// console.log(database[0].c);
 
 const getcontainer = document.querySelector('.container');
 const getquestion = document.querySelector('.question');
-const getanswer = document.querySelector('.answer');
-// console.log(getanswer);
+const getanswers = document.querySelectorAll('.answer');
+// console.log(getanswers);
 
-const geta_img = document.getElementById('a_img')
-const getb_img = document.getElementById('b_img')
-const getc_img = document.getElementById('c_img')
-const getd_img = document.getElementById('d_img')
-// console.log(geta_img);
+const geta_img = document.getElementById('a_img'),
+      getb_img = document.getElementById('b_img'),
+      getc_img = document.getElementById('c_img'),
+      getd_img = document.getElementById('d_img');
+
+      // console.log(geta_img)
+const getbtn = document.querySelector('.btn');
+
+
+let currentidx = 0;
+let score = 0;
+
 
 startquestion();
-
 function startquestion(){
-    getquestion.textContent = database[0].question;
-    geta_img.src = database[0].a;
-    getb_img.src = database[0].b;
-    getc_img.src = database[0].c;
-    getd_img.src = database[0].d;
+
+    const currentqes = database[currentidx];
+    // console.log(currentqes);
+
+    getquestion.textContent = currentqes.question;
+    geta_img.src = currentqes.a;
+    getb_img.src = currentqes.b;
+    getc_img.src = currentqes.c;
+    getd_img.src = currentqes.d;
 }
 
-// 19BT
+function getsingleanswer(){
+    let answer;
+    // console.log(answer);
+    // console.log(getanswer);
+
+    getanswers.forEach(function(getanswer){
+        // console.log(getanswer);
+        // console.log(getanswer.id);
+
+        if(getanswer.checked){
+            // console.log(getanswer.id)
+            answer = getanswer.id;
+        }
+    });
+    // console.log(answer);
+    return answer;
+}
+
+// getsingleanswer()
+
+
+
+getbtn.addEventListener('click',function(){
+    // console.log('i am working');
+
+    const answer = getsingleanswer();
+
+    // console.log(answer);
+
+    if (answer) {
+        // console.log(answer);
+
+        if (answer === database[currentidx].correctanswer) {
+            score++;
+        }
+
+        currentidx++;
+
+        // console.log(currentidx);
+
+        if (currentidx < database.length) {
+            startquestion();
+        } else {
+            // console.log(score);
+
+            getcontainer.innerHTML = `
+            <h3>Total Score : ${score * 25}</h3>
+            <h4>Your answer correctly at ${score} /${database.length}</h4>
+            <button type="button" class="btn" ondblclick="location.reload()" > CLick to Reload</button>
+            `;
+
+
+        }
+
+
+    } else {
+        window.alert('Choose One Answer');
+    }
+
+});
+
+function removeselects(){
+    getanswers.forEach(function(getanswer){
+        return getanswer.checked = false;
+    });
+}
